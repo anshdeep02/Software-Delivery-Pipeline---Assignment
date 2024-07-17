e966dc65302e39ed50e59b9e0474951de8bf3709
anshdeep02 <anshdeep.singh@think41.com>
2024-07-17 16:57:35 +0530

    add docker files

--- backend/src/api/ask.js
+++ backend/src/api/ask.js
@@ -4,26 +4,6 @@ import { getGroqChatCompletion } from "../utils/groq.js";
 
 const router = express.Router();
 
-// router.get("/ask", async (req, res) => {
-//   const question = req.query.question;
-
-//   // Check if question is provided
-//   if (!question) {
-//     return res.status(400).send({ message: "Missing question parameter" });
-//   }
-
-//   try {
-//     // Call the Groq service to get LLM response with the question
-//     console.log(question)
-//     const llmResponse = await getGroqChatCompletion(question);
-//     // Send the LLM response back to the frontend
-//     res.json({ response: llmResponse });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ message: "Internal server error" });
-//   }
-// });
-
 router.post("/ask", async (req, res) => {
   const question = req?.body?.text;
 
